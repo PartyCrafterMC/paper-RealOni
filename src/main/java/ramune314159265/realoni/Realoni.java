2574dead99bec5ae73fd5c5e5ac0bc514e29dbda
ramune314159265 <ramune314159265@gmail.com>
2025-07-26 00:32:52 +0900

    ワールド初期化

--- src/main/java/ramune314159265/realoni/Realoni.java
+++ src/main/java/ramune314159265/realoni/Realoni.java
@@ -1,11 +1,14 @@
 package ramune314159265.realoni;
 
+import org.bukkit.Difficulty;
 import org.bukkit.GameMode;
+import org.bukkit.GameRule;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Realoni extends JavaPlugin {
+	public static final int worldSize = 600;
 	public static World defaultWorld;
 	private static Realoni instance;
 
@@ -14,6 +17,17 @@ public final class Realoni extends JavaPlugin {
 	}
 
 	public static void worldInitialize() {
+		defaultWorld.setTime(1000);
+		defaultWorld.setDifficulty(Difficulty.PEACEFUL);
+		defaultWorld.setGameRule(GameRule.SHOW_DEATH_MESSAGES, false);
+		defaultWorld.setGameRule(GameRule.DO_DAYLIGHT_CYCLE,false);
+		defaultWorld.setGameRule(GameRule.DO_WEATHER_CYCLE,false);
+		defaultWorld.setGameRule(GameRule.COMMAND_BLOCK_OUTPUT,false);
+		defaultWorld.setGameRule(GameRule.KEEP_INVENTORY,true);
+		defaultWorld.setGameRule(GameRule.SPAWN_RADIUS,0);
+		defaultWorld.getWorldBorder().setDamageAmount(0);
+		defaultWorld.getWorldBorder().setCenter(0,0);
+		defaultWorld.getWorldBorder().setSize(worldSize);
 		InitialRoom.place();
 	}
 
