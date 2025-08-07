d318fdf6a47b89f21c767a4ad79b048b93e00171
ramune314159265 <ramune314159265@gmail.com>
2025-08-07 23:55:14 +0900

    判定方法をisPassableに

--- src/main/java/ramune314159265/realoni/Ground.java
+++ src/main/java/ramune314159265/realoni/Ground.java
@@ -12,21 +12,11 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Ground {
-	private static final Set<Material> IGNORED_BLOCKS = Sets.newHashSet(Iterables.concat(
-			Tag.FLOWERS.getValues(),
-			Tag.LEAVES.getValues(),
-			Tag.REPLACEABLE.getValues(),
-			new HashSet<>(Arrays.asList(
-					Material.SUGAR_CANE,
-					Material.GLASS
-			))));
-
 	public static int getY(World world, int x, int z) {
 		for (int y = world.getMaxHeight() - 1; y >= 0; y--) {
 			Block block = world.getBlockAt(x, y, z);
-			Material type = block.getType();
 
-			if (type.isSolid() && !IGNORED_BLOCKS.contains(type)) {
+			if (!block.isPassable()) {
 				return y;
 			}
 		}
