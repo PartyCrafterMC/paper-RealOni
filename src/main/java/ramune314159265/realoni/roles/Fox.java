0a18d3e8bdd957d42673b13a600d95ff05db6efd
ramune314159265 <ramune314159265@gmail.com>
2025-12-22 00:02:49 +0900

    エフェクトなど

--- src/main/java/ramune314159265/realoni/roles/Fox.java
+++ src/main/java/ramune314159265/realoni/roles/Fox.java
@@ -1,7 +1,14 @@
 package ramune314159265.realoni.roles;
 
 import dev.iiahmed.disguise.Disguise;
+import org.bukkit.NamespacedKey;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import ramune314159265.realoni.Realoni;
 import ramune314159265.realoni.skills.*;
 
 import java.util.List;
@@ -16,6 +23,19 @@ public class Fox extends Oni{
 		super.initialize();
 
 		setInventory();
+		player.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, -1, 0, false, false, true));
+		AttributeInstance jumpAttribute = player.getAttribute(Attribute.JUMP_STRENGTH);
+		jumpAttribute.addModifier(new AttributeModifier(NamespacedKey.fromString("fox_jump", Realoni.getInstance()), 0.75d, AttributeModifier.Operation.MULTIPLY_SCALAR_1));
+	}
+
+	@Override
+	public void exit() {
+		try {
+			super.exit();
+			player.removePotionEffect(PotionEffectType.FIRE_RESISTANCE);
+			player.getAttribute(Attribute.JUMP_STRENGTH).removeModifier(NamespacedKey.fromString("fox_jump", Realoni.getInstance()));
+		} catch (Exception ignored) {
+		}
 	}
 
 	public void setInventory() {}
